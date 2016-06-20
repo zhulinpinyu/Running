c7174bf4ba439fe673eab6ecdb9c4e1c2da5caf7
zhulinpinyu <mulixiang199016@gmail.com>
2016-06-20 18:31:58 +0800

    show apple map

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,36 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
+  MapView,
   View
 } from 'react-native';
 
 class Running extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
+    render() {
+          return (
+            <View style={styles.container}>
+              <MapView
+                  showsUserLocation={true}
+                  followUserLocation={true}
+                  style={styles.map}
+                  />
+            </View>
+          );
+      }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'stretch',
+        backgroundColor: '#F5FCFF',
+    },
+    map:{
+        flex:1
+    }
 });
 
 AppRegistry.registerComponent('Running', () => Running);
