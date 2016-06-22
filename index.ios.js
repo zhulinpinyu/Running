4cadbaa9212f198125b0dfe2243d7c2fc081ad76
zhulinpinyu <mulixiang199016@gmail.com>
2016-06-23 01:00:40 +0800

    router distance [计算运动路径的距离]

--- index.ios.js
+++ index.ios.js
@@ -7,6 +7,8 @@ import {
   View
 } from 'react-native'
 
+import geolib from 'geolib'
+
 class Running extends Component {
   constructor(){
     super()
@@ -31,8 +33,10 @@ class Running extends Component {
         const {latitude,longitude,accuracy} = position.coords
         this.setState({...this.state, info: position.coords})
         const coord = {latitude: latitude-0.002716, longitude: longitude+0.005055}
+        //const coord = {latitude: latitude, longitude: longitude}
         const footprintCoords = this.state.footprintCoords.concat([coord])
         const startPosition = [footprintCoords[0]]
+        this._distance(footprintCoords)
         this.setState({...this.state,startPosition,footprintCoords})
       },
       (error) => alert(error.message),
@@ -40,12 +44,19 @@ class Running extends Component {
     )
   }
 
-  // _distance(coords){
-  //   let distance = 0
-  //   for(let i=0;i<coords.length-1,i++){
-  //     distance += (coords[i],coords[i+1])
-  //   }
-  // }
+  _distance(coords){
+    let distance = 0
+    if(coords.length > 2){
+      for(let i=0;i<coords.length-1;i++){
+        const lat0 = coords[i].latitude
+        const lon0 = coords[i].longitude
+        const lat1 = coords[i+1].latitude
+        const lon1 = coords[i+1].longitude
+        distance += geolib.getDistance({latitude: lat0,longitude: lon0},{latitude: lat1,longitude: lon1})
+      }
+    }
+    this.setState({...this.state,info: {...this.state.info,distance:distance}})
+  }
   render() {
         return (
           <View style={styles.container}>
